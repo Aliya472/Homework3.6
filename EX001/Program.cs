73f98848ed01e486695ca041296c25be7da5b184
alya <alya-892@yandex.ru>
2022-11-11 19:40:19 +0300

    Задание1

--- EX001/Program.cs
+++ EX001/Program.cs
@@ -27,18 +27,3 @@ int Count(int[] text)
 int r=Count(i); 
 Console.WriteLine(r); 
 
-
-// Console.WriteLine("Введите число через пробел"); 
-// int[] Num = Array.ConvertAll(Console.ReadLine().Split(), int.Parse); 
-// for (int i = 0; i < Num.Length; i++) 
-// { 
-//     if (Num[i] > 0) Count++; 
-// } 
-// Console.WriteLine(Count); 
- 
- 
-// 2. печать в строку написанные числа 
-// 3. счетчик =0 
-// 4. Решение 
-// 5. вывод 
-
